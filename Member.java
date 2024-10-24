c74ca4dab541492964482a4dbebe7efe91692a7b
Nour zeidan <zeidann9@gmail.com>
2024-10-24 23:51:38 +0300

    Add files via upload

--- Member.java
+++ Member.java
@@ -1,24 +1,22 @@
 public class Member extends Person {
+
     private String memberID;
-    private String name;
     private String membershipType;
-    private String email;
-    private String phoneNumber;
     private String status;
     public Member(String memberID, String name, String membershipType, String email, String phoneNumber, String status) {
+        super(name, email, phoneNumber);
         this.memberID = memberID;
-        this.name = name;
         this.membershipType = membershipType;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
         this.status = status;
     }
+
     @Override
     public String lineRepresentation () {
         String line = String.join(",", memberID, name, membershipType, email, phoneNumber, status);
         //System.out.println(line);
         return line;
     }
+
     @Override
     public String getSearchKey() {
         return memberID;
