9ffb06f7656904c7670eeb45cf8f3e43fba24b48
Nour zeidan <zeidann9@gmail.com>
2024-10-24 23:52:17 +0300

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,4 +1,15 @@
 abstract class Person {
+
+    protected String name;
+    protected String email;
+    protected String phoneNumber;
+
+    public Person(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
     protected abstract String getSearchKey();
 
     protected abstract String lineRepresentation();
