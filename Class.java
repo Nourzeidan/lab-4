934aecbf287f895fe4d376822ebef8cecde8fd08
Nour zeidan <zeidann9@gmail.com>
2024-10-24 14:28:28 +0300

    Add files via upload

--- Class.java
+++ Class.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Class {
+public class Class extends Person{
 
     private String classId;
     private String className;
@@ -25,6 +25,7 @@ public class Class {
         return availableSeats;
     }
 
+    @Override
     public String lineRepresentation()
     {
         String durationS = Integer.toString(duration);
@@ -33,6 +34,7 @@ public class Class {
         return String.join(",",arr);
 
     }
+    @Override
     public String getSearchKey() {
         return classId;
     }
