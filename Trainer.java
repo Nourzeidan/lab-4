56bd2d31d3f7e942c25b4e69ccbd79142247ab8f
Nour zeidan <zeidann9@gmail.com>
2024-10-24 23:51:58 +0300

    Add files via upload

--- Trainer.java
+++ Trainer.java
@@ -6,18 +6,14 @@ public class Trainer extends Person
 {
 
     private String trainerId;
-    private String name;
-    private String email;
     private String speciality;
-    private String phoneNumber;
 
     public Trainer(String trainerId, String name, String email, String speciality, String phoneNumber)
     {
+        super(name, email, phoneNumber);
         this.trainerId = trainerId;
-        this.name = name;
-        this.email = email;
         this.speciality = speciality;
-        this.phoneNumber = phoneNumber;
+
     }
 
     @Override
