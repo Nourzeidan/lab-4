3447cdc4739b89eb979e2e1512fdb2b0d2647ce4
Nour zeidan <zeidann9@gmail.com>
2024-10-25 14:47:41 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@
 //
 
 import java.io.IOException;
+//import java.lang.Class;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
-
+//import Class as gymClass;
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        TrainerDatabase database = new TrainerDatabase("Trainers.txt");
-        database.readFromFile();
-        System.out.println("Please select your speciality:\n1-Admin\n2-Trainer\n3-Member");
+
+        System.out.println("Please select your speciality:\n1-Admin\n2-Trainer\n");
         int check = scan.nextInt();
         int choice;
         String Memberid;
@@ -27,8 +27,8 @@ public class Main {
         String phoneNumber;
         String trainer;
         if (check == 1) {
-            AdminRole admin = new AdminRole(database);
-
+            TrainerDatabase database = new TrainerDatabase("Trainers.txt");
+            AdminRole admin = new AdminRole();
             do {
                 System.out.println("1. Add Trainer");
                 System.out.println("2. View All Trainers");
@@ -104,128 +104,107 @@ public class Main {
         } else {
             String searchKey;
             String deleteId;
-            if (check == 2) {
-                MemberDatabase Mdatabase = new MemberDatabase("Members.txt");
-                Mdatabase.readFromFile();
-
-                do {
-                    System.out.print("Please Select a number\n1-Add Member\n2-Delete Member\n3-Search Member\n4-View All members\n");
-                    choice = scan.nextInt();
-                    switch (choice) {
-                        case 1:
-                            System.out.print("Enter Member ID: ");
-                            scan.nextLine();
-                            Memberid = scan.nextLine();
-                            System.out.print("Enter Name: ");
-                            classID = scan.nextLine();
-                            System.out.print("Enter MembershipType: ");
-                            status = scan.nextLine();
-                            System.out.print("Enter Email: ");
-                            date = scan.nextLine();
-                            System.out.print("Enter Phone Number: ");
-                            phoneNumber = scan.nextLine();
-                            System.out.print("Enter Member status: ");
-                            searchKey = scan.nextLine();
-                            Member member = new Member(Memberid, classID, status, date, phoneNumber, searchKey);
-                            Mdatabase.insertRecord(member);
-                            break;
-                        case 2:
-                            System.out.print("Enter Member ID to delete: ");
-                            scan.nextLine();
-                            deleteId = scan.nextLine();
-                            Member foundMember = Mdatabase.getRecord(deleteId);
-                            if (foundMember != null) {
-                                System.out.println("Member found: " + foundMember.lineRepresentation());
-                                Mdatabase.deleteRecord(deleteId);
-                            }
-                            break;
-                        case 3:
-                            System.out.print("Enter Member ID to search: ");
-                            scan.nextLine();
-                            trainer = scan.nextLine();
-                            Member key = Mdatabase.getRecord(trainer);
-                            if (key != null) {
-                                System.out.println("Member found: " + key.lineRepresentation());
-                            } else {
-                                System.out.println("Member not found.");
-                            }
-                            break;
-                        case 4:
-                            ArrayList<Member> members = Mdatabase.returnAllRecords();
-                            Iterator var18 = members.iterator();
-
-                            while(var18.hasNext()) {
-                                Member m = (Member)var18.next();
-                                System.out.println(m.lineRepresentation());
-                            }
-                    }
-
-                    System.out.println("1-Continue\n2-Exit");
-                    check = scan.nextInt();
-                } while(check == 1);
-            } else if (check == 3) {
-                MemberClassRegistrationDatabase MCDataBase = new MemberClassRegistrationDatabase("Registrations.txt");
-                MCDataBase.readFromFile();
-
-                do {
-                    System.out.println("1. Register for a class");
-                    System.out.println("2. View All Registrations");
-                    System.out.println("3. Search for a Registration Record");
-                    System.out.println("4. Delete Registration by ID");
-                    System.out.println("Choose an option: ");
-                    choice = scan.nextInt();
-                    scan.nextLine();
-                    MemberClassRegistration Registration;
-                    label65:
-                    switch (choice) {
-                        case 1:
-                            System.out.print("Enter Member ID: ");
-                            Memberid = scan.nextLine();
-                            System.out.print("Enter Class ID: ");
-                            classID = scan.nextLine();
-                            System.out.print("Enter Status: ");
-                            status = scan.nextLine();
-                            System.out.print("Enter Date: ");
-                            date = scan.nextLine();
-                            MCDataBase.insertRecord(new MemberClassRegistration(Memberid, classID, status, LocalDate.parse(date)));
-                            break;
-                        case 2:
-                            ArrayList<MemberClassRegistration> Registrations = MCDataBase.returnAllRecords();
-                            System.out.println("List of Registrations:");
-                            Iterator var25 = Registrations.iterator();
-
-                            while(true) {
-                                if (!var25.hasNext()) {
-                                    break label65;
-                                }
-
-                                Registration = (MemberClassRegistration)var25.next();
-                                System.out.println(Registration.lineRepresentation());
-                            }
-                        case 3:
-                            System.out.print("Enter Member ID and Class ID to search: ");
-                            searchKey = scan.nextLine();
-                            Registration = MCDataBase.getRecord(searchKey);
-                            if (Registration != null) {
-                                System.out.println("Registration found: " + Registration.lineRepresentation());
-                            } else {
-                                System.out.println("Registration not found.");
-                            }
-                            break;
-                        case 4:
-                            System.out.print("Enter MemberID and ClassID to delete: ");
-                            deleteId = scan.nextLine();
-                            MCDataBase.deleteRecord(deleteId);
-                            break;
-                        default:
-                            System.out.println("Invalid option. Please try again.");
-                    }
-
-                    System.out.println("1-Continue\n2-Exit");
-                    check = scan.nextInt();
-                } while(check == 1);
-            }
+            MemberDatabase MemberDatabase = new MemberDatabase("Members.txt");
+            MemberClassRegistrationDatabase RegistrationDatabase = new MemberClassRegistrationDatabase("Registrations.txt");
+            TrainerRole trainerRole= new TrainerRole();
+            do {
+                System.out.print("Please Select a number\n1-Add Member\n2-Delete Member\n3-Search Member\n4-View All members\n5-View All Classes\n6-Register for class\n7-Search for Registration\n8-View All registrations\n9-cancel Registration");
+                choice = scan.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Member ID: ");
+                        scan.nextLine();
+                        Memberid = scan.nextLine();
+                        System.out.print("Enter Name: ");
+                        classID = scan.nextLine();
+                        System.out.print("Enter MembershipType: ");
+                        status = scan.nextLine();
+                        System.out.print("Enter Email: ");
+                        date = scan.nextLine();
+                        System.out.print("Enter Phone Number: ");
+                        phoneNumber = scan.nextLine();
+                        System.out.print("Enter Member status: ");
+                        status = scan.nextLine();
+                        //Member member = new Member(Memberid, classID, status, date, phoneNumber, status);
+                        trainerRole.addMember(Memberid, classID, status, date, phoneNumber,status);
+                        break;
+                    case 2:
+                        System.out.print("Enter Member ID to delete: ");
+                        scan.nextLine();
+                        deleteId = scan.nextLine();
+                        Member foundMember = MemberDatabase.getRecord(deleteId);
+                        if (foundMember != null) {
+                            System.out.println("Member found: " + foundMember.lineRepresentation());
+                            MemberDatabase.deleteRecord(deleteId);
+                        }
+                        break;
+                    case 3:
+                        System.out.print("Enter Member ID to search: ");
+                        scan.nextLine();
+                        trainer = scan.nextLine();
+                        Member key = MemberDatabase.getRecord(trainer);
+                        if (key != null) {
+                            System.out.println("Member found: " + key.lineRepresentation());
+                        } else {
+                            System.out.println("Member not found.");
+                        }
+                        break;
+                    case 4:
+                        ArrayList<Member> members =trainerRole.getListOfMembers();
+                        for (Member m : members) {
+                            System.out.println(m.lineRepresentation());
+                        }
+                        break;
+                    case 5:
+                        System.out.print("All Available Classes:");
+                        ArrayList<Class> classes=trainerRole.getListOfClasses();
+                        for (Class c : classes) {
+                            System.out.println(c.lineRepresentation());
+                        }
+                        break;
+                    case 6:
+                        System.out.print("Please enter the following information to register for class: ");
+                        System.out.println("Please enter member ID:");
+                        String memberID = scan.nextLine();
+                        System.out.println("Please enter the Class ID:");
+                        classID = scan.nextLine();
+                        System.out.println("Please enter the Status:");
+                        status = scan.nextLine();
+                        System.out.println("Please enter the Date:");
+                        date = scan.nextLine();
+                        trainerRole.registerMemberForClass(memberID,classID,LocalDate.parse(date));
+                        break;
+                    case 7:
+                        System.out.print("Enter Member ID and Class ID to search: ");
+                        searchKey = scan.nextLine();
+                        MemberClassRegistration Registration = RegistrationDatabase.getRecord(searchKey);
+                        if (Registration != null) {
+                            System.out.println("Registration found: " + Registration.lineRepresentation());
+                        } else {
+                            System.out.println("Registration not found.");
+                        }
+                        break;
+                    case 8:
+                        System.out.print("Registrations:\n");
+                        ArrayList<MemberClassRegistration> records=trainerRole.getListOfRegistrations();
+                        for(MemberClassRegistration mc : records) {
+                            System.out.println(mc.lineRepresentation());
+                        }
+                        break;
+                    case 9:
+                        System.out.print("Please Enter Member ID to delete: ");
+                        String deleteID=scan.nextLine();
+                        System.out.println("Please enter the Class ID to delete:");
+                        String deleteClassID=scan.nextLine();
+                        trainerRole.cancelRegistration(deleteID,deleteClassID);
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                }
+                System.out.println("1-Continue\n2-Exit");
+                check = scan.nextInt();
+            } while(check == 1);
         }
-
     }
+
 }
