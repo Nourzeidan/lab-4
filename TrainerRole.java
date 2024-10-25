c17308be9f21340493dad0244368d8b4b8b9aa01
Nour zeidan <zeidann9@gmail.com>
2024-10-25 14:48:58 +0300

    Add files via upload

--- TrainerRole.java
+++ TrainerRole.java
@@ -1,7 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,10 +7,11 @@ import java.util.ArrayList;
 
 public class TrainerRole {
     private MemberClassRegistrationDatabase memberClassRegistrationDatabase;
-    private MemberDatabase memberDatabase = new MemberDatabase("Members.txt");
+    private MemberDatabase memberDatabase;
     private ClassDatabase classDatabase;
 
-    public TrainerRole() throws FileNotFoundException {
+    public TrainerRole() throws IOException {
+        this.memberDatabase = new MemberDatabase("Members.txt");
         this.memberDatabase.readFromFile();
         this.classDatabase = new ClassDatabase("Class.txt");
         this.classDatabase.readFromFile();
@@ -22,68 +19,73 @@ public class TrainerRole {
         this.memberClassRegistrationDatabase.readFromFile();
     }
 
+    public Member SearchMember(String memberID) {
+        return memberDatabase.getRecord(memberID);
+    }
+
+    public MemberClassRegistration getMemberClassRegistration(String Search) {
+        return memberClassRegistrationDatabase.getRecord(Search);
+    }
     public void addMember(String memberID, String name, String membershipType, String email, String phoneNumber, String status) throws IOException {
         Member m = new Member(memberID, name, membershipType, email, phoneNumber, status);
         this.memberDatabase.insertRecord(m);
     }
 
     public ArrayList<Member> getListOfMembers() {
-        return this.memberDatabase.returnAllRecords();
+        return new ArrayList<>(this.memberDatabase.returnAllRecords()); // Return a copy for encapsulation
     }
 
-    public void addClass(String memberID, String name, String membershipType, String email, String phoneNumber, String status) throws IOException {
-        Member m = new Member(memberID, name, membershipType, email, phoneNumber, status);
-        this.memberDatabase.insertRecord(m);
+    public void addClass(String classID, String name, String trainerID, int duration, int availableSeats) throws IOException {
+        Class c = new Class(classID, name, trainerID, duration, availableSeats);
+        this.classDatabase.insertRecord(c);
     }
 
     public ArrayList<Class> getListOfClasses() {
-        return this.classDatabase.returnAllRecords();
+        return new ArrayList<>(this.classDatabase.returnAllRecords()); // Return a copy for encapsulation
     }
 
     public boolean registerMemberForClass(String memberID, String classID, LocalDate registrationDate) throws IOException {
-        boolean check = this.classDatabase.contains(classID);
-        if (check) {
+        if (this.classDatabase.contains(classID)) {
             Class record = this.classDatabase.getRecord(classID);
-            if (record.getAvailableSeats() != 0) {
-                MemberClassRegistration m = new MemberClassRegistration(memberID, classID, "active", registrationDate);
-                this.memberClassRegistrationDatabase.insertRecord(m);
+            if (record.getAvailableSeats() > 0) {
+                MemberClassRegistration registration = new MemberClassRegistration(memberID, classID, "active", registrationDate);
+                this.memberClassRegistrationDatabase.insertRecord(registration);
                 record.setAvailableSeats(record.getAvailableSeats() - 1);
                 return true;
+            } else {
+                System.out.println("Class " + classID + " has no seats available");
             }
-
-            System.out.println("Class " + classID + " has no seats available");
         } else {
             System.out.println("Class does not exist");
         }
-
         return false;
     }
 
     public boolean cancelRegistration(String memberID, String classID) throws IOException {
         MemberClassRegistration record = this.memberClassRegistrationDatabase.getRecord(memberID + classID);
-        LocalDate date = record.getRegistrationDate();
+        if (record == null) {
+            System.out.println("Registration does not exist");
+            return false;
+        }
+
+        LocalDate registrationDate = record.getRegistrationDate();
         LocalDate today = LocalDate.now();
-        long diffdays = ChronoUnit.DAYS.between(date, today);
-        if (diffdays <= 3L) {
+        long daysSinceRegistration = ChronoUnit.DAYS.between(registrationDate, today);
+
+        if (daysSinceRegistration <= 3) {
             Class registeredClass = this.classDatabase.getRecord(classID);
-            if (record != null) {
-                record.setStatus("Canceled");
-                registeredClass.setAvailableSeats(registeredClass.getAvailableSeats() + 1);
-                System.out.println(memberID + classID + " has canceled registration and issued a refund");
-                return true;
-            } else {
-                System.out.println("Registration does not exist");
-                return false;
-            }
+            record.setStatus("Canceled");
+            registeredClass.setAvailableSeats(registeredClass.getAvailableSeats() + 1);
+            System.out.println("Registration for " + memberID + " in class " + classID + " has been canceled and a refund issued.");
+            return true;
         } else {
-            System.out.println("You cannot issue a refund,registration is done more than 3 days ago.");
+            System.out.println("Cannot issue a refund. The registration was completed more than 3 days ago.");
             return false;
         }
     }
 
     public ArrayList<MemberClassRegistration> getListOfRegistrations() throws IOException {
-        ArrayList<MemberClassRegistration> records = this.memberClassRegistrationDatabase.returnAllRecords();
-        return records;
+        return new ArrayList<>(this.memberClassRegistrationDatabase.returnAllRecords()); // Return a copy for encapsulation
     }
 
     public void logout() throws IOException {
