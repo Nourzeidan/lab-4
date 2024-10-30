c426f2ae6e9c25ff2d9cdf44577b297bf88be716
Nour zeidan <zeidann9@gmail.com>
2024-10-30 13:46:20 +0300

    Add files via upload

--- MemberDatabase.java
+++ MemberDatabase.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MemberDatabase extends Database<Member> {
+public class MemberDatabase extends Database<Member>{
 
     public MemberDatabase(String filename) {
         super(filename);
@@ -12,8 +12,7 @@ public class MemberDatabase extends Database<Member> {
     public Member createRecordFrom (String line){
         String[] parts=line.split(",");
         if(parts.length==6) {
-            Member member = new Member(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
-            return member;
+            return (Member) new Member(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
         }
         else {
             System.out.println("Error,Line incomplete");
@@ -22,12 +21,12 @@ public class MemberDatabase extends Database<Member> {
     }
 
     @Override
-    protected String getSearchKey(Member record) {
+    public String getSearchKey(Member record) {
         return record.getSearchKey();
     }
 
     @Override
-    protected String lineRepresentation(Member record) {
+    public String lineRepresentation(Member record) {
         return record.lineRepresentation();
     }
 
